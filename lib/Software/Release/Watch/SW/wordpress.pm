0a90d61f5e123f62ba9cf16e49d7e11122e263a9
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-11-08 17:06:14 +0700

    Release v0.01

--- lib/Software/Release/Watch/SW/wordpress.pm
+++ lib/Software/Release/Watch/SW/wordpress.pm
@@ -3,13 +3,23 @@ package Software::Release::Watch::SW::wordpress;
 use 5.010;
 use Moo;
 
+# VERSION
+
 with 'Software::Release::Watch::Source::WebPage::ArchiveLinks';
 with 'Software::Release::Watch::Versioning::SemVer';
+with 'Software::Release::Watch::Software';
 
-sub releases_url     { "http://wordpress.org/download/release-archive/" }
-sub tarball_name_pat { qr!/-(?<v>.+)$! }
+sub url { "http://wordpress.org/download/release-archive/" }
+
+sub filter_filename {
+    my ($self, $fn) = @_;
+    $fn =~ /^wordpress-(?=\d)/o;
+}
+
 
 1;
 # ABSTRACT: Watch wordpress releases
 
 =cut
+
+=for Pod::Coverage .+
