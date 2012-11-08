bc5a426cd768853e686381ccb8183b0f66cb4667
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-11-08 13:57:32 +0700

    Initial commit

---
+++ lib/Software/Release/Watch/wordpress.pm
@@ -0,0 +1,21 @@
+package Software::Release::Watch::wordpress;
+
+use 5.010;
+use Moo;
+
+with 'Software::Release::Watch::Software';
+with 'Software::Release::Watch::HasReleasesInURL';
+
+sub releases_url     { "http://wordpress.org/download/release-archive/" }
+sub tarball_name_pat { qr!/-(?<v>.+)$! }
+
+1;
+# ABSTRACT: Watch wordpress releases
+
+=head1 SYNOPSIS
+
+ use Software::Release::Watch qw(list_software_releases);
+
+ my $res = list_software_releases(software_id=>'wordpress');
+
+=cut
